018e703d80bc6b50bfd36f78464d93cd39ead933
Monkey <Dert.thatch@gmail.com>
2022-10-06 23:38:34 +0700

    EventCallback and Form Validations

--- BlazorApp/Data/Grocery.cs
+++ BlazorApp/Data/Grocery.cs
@@ -1,8 +1,15 @@
-﻿namespace BlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should less than 15 digits")]
         public string Name { get; set; }
+        
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Should have value greater than 0")]
         public float Price { get; set; }
 
     }
